17ae402fe9de42a7e03de31b63ca2bdab57fce9b
Michaël Vanderheyden <michael.vanderheyden@atos.net>
2022-03-28 16:21:16 +0200

    fix: allow blank issue
    worakaround the fact that issue forms are in beta and currently only available for public repos
    https://docs.github.com/en/communities/using-templates-to-encourage-useful-issues-and-pull-requests/configuring-issue-templates-for-your-repository#creating-issue-forms

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
-		'body-max-line-length': [2, 'always', 140],
+		'body-max-line-length': [1, 'always', 140],
 		'footer-max-line-length': [2, 'always', 140],
 		'header-max-length': [2, 'always', 140]
 	}
